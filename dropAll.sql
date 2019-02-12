6bffa2132afd386b0a3af63a08e96822c7e36c98
SStreich <selene.streich@gmail.com>
2019-02-12 01:21:06 +0100

    Add dropAll.sql for fast dropping all tables

---
+++ dropAll.sql
@@ -0,0 +1,21 @@
+drop table account_playlist cascade ;
+
+drop table account cascade ;
+
+drop table artist_album cascade ;
+
+drop table album cascade ;
+
+drop table playlist cascade ;
+
+drop table song_genre cascade ;
+
+drop table genre cascade ;
+
+drop table song_playlist cascade ;
+
+drop table artist_song cascade ;
+
+drop table artist cascade ;
+
+drop table song cascade ;
