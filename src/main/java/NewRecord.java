b9199d7ea571b5e77f5a635bad8e99e02a1b7167
SStreich <selene.streich@gmail.com>
2019-02-19 10:30:30 +0100

    Add final version of addNewRecord method

--- src/main/java/NewRecord.java
+++ src/main/java/NewRecord.java
@@ -2,22 +2,27 @@ import java.sql.*;
 
 public class NewRecord {
     DAO dao = new DAO();
+    //static int artist_id = 1;
+    static int album_id = 1;
+    static int song_id = 1;
+    static int genre_id = 1;
 
-    void addNewRecord(String song, String artist, String album, String year, String genre) throws SQLException {
+    void addNewRecord(String song, String album, String artist, String genre, String year) throws SQLException {
 
         dao.addArtist(artist);
-        int artist_id = dao.getRecordResultSet("artist", artist).getInt(0);
+        int artist_id = dao.getRecordId("artist", artist, "artist_id");
 
         dao.addGenre(genre);
-        int genre_id = dao.getRecordResultSet("genre", genre).getInt(0);
+        int genre_id = dao.getRecordId("genre", genre, "genre_id");
 
         dao.addAlbum(album, artist, year);
-        int album_id = dao.getAlbumResultSet(album, artist).getInt(0);
+        int album_id = dao.getRecordId("album", album, "album_id");
+        dao.addToArtist_Album(artist_id, album_id);
+
 
         dao.addSong(song, artist, album_id);
-        int song_id = dao.getSongResultSet(song, artist).getInt(0);
+        int song_id = dao.getRecordId("song", song, "song_id");
 
-        dao.addToArtist_Album(artist_id, album_id);
         dao.addToArtist_Song(artist_id, song_id);
         dao.addToSong_Genre(song_id, genre_id);
     }
