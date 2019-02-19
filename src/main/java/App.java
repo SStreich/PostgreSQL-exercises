391007573e539f755810527e2c775f3639f95680
SStreich <selene.streich@gmail.com>
2019-02-19 10:32:47 +0100

    Add final version of main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class App {
+
+    public static void main(String[] args) throws SQLException {
+        CSVController csvController = new CSVController("src/main/resources/music-data-159680.csv");
+        ArrayList<String[]> records = csvController.getRecords();
+        NewRecord newRecord = new NewRecord();
+        int i = 0;
+
+        for (String[] record : records) {
+             newRecord.addNewRecord(record[0], record[1], record[2], record[3], record[4]);
+            System.out.println(i++);
+        }
+
+    }
 }
