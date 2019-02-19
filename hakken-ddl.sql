f853674f39b65eaacd99d7330cd252db27716c51
SStreich <selene.streich@gmail.com>
2019-02-19 11:53:51 +0100

    Change queries to create anything only if it not exists, create simple index

--- hakken-ddl.sql
+++ hakken-ddl.sql
@@ -1,5 +1,5 @@
 -- 1
-CREATE TABLE account (
+CREATE TABLE IF NOT EXISTS account (
   account_id SERIAL PRIMARY KEY,
   name VARCHAR(70) NOT NULL,
   password VARCHAR(50) NOT NULL,
@@ -7,20 +7,20 @@ CREATE TABLE account (
 );
 
 -- 2
-CREATE TABLE artist (
+CREATE TABLE IF NOT EXISTS  artist (
   artist_id SERIAL PRIMARY KEY,
   name VARCHAR(999) NOT NULL
 );
 
 -- 3
-CREATE TABLE album (
+CREATE TABLE IF NOT EXISTS album (
   album_id SERIAL PRIMARY KEY,
   name VARCHAR(990) NOT NULL ,
   year VARCHAR(30)
 );
 
 -- 4
-CREATE TABLE song (
+CREATE TABLE IF NOT EXISTS song (
   song_id SERIAL PRIMARY KEY,
   name VARCHAR(990) NOT NULL,
   album_id INT,
@@ -28,48 +28,48 @@ CREATE TABLE song (
 );
 
 -- 5
-CREATE TABLE playlist (
+CREATE TABLE IF NOT EXISTS playlist (
   playlist_id SERIAL PRIMARY KEY,
   name VARCHAR(70) NOT NULL,
   owner_id INTEGER NOT NULL
 );
 
 -- 6
-CREATE TABLE genre (
+CREATE TABLE IF NOT EXISTS genre (
   genre_id SERIAL PRIMARY KEY,
   name VARCHAR(70) UNIQUE NOT NULL
 );
 
 -- 7
-CREATE TABLE account_playlist (
+CREATE TABLE IF NOT EXISTS account_playlist (
   account_id INTEGER NOT NULL REFERENCES account,
   playlist_id INTEGER NOT NULL REFERENCES playlist,
   PRIMARY KEY (account_id, playlist_id)
 );
 
 -- 8
-CREATE TABLE song_genre (
+CREATE TABLE IF NOT EXISTS song_genre (
   song_id INTEGER NOT NULL REFERENCES song,
   genre_id INTEGER NOT NULL REFERENCES genre,
   PRIMARY KEY (song_id, genre_id)
 );
 
 -- 9
-CREATE TABLE song_playlist (
+CREATE TABLE IF NOT EXISTS song_playlist (
   song_id INTEGER NOT NULL REFERENCES song,
   playlist_id INTEGER NOT NULL REFERENCES playlist,
   PRIMARY KEY (song_id, playlist_id)
 );
 
 -- 10
-CREATE TABLE artist_song (
+CREATE TABLE IF NOT EXISTS artist_song (
   artist_id INTEGER NOT NULL REFERENCES artist,
   song_id INTEGER NOT NULL REFERENCES song,
   PRIMARY KEY (artist_id, song_id)
 );
 
 -- 11
-CREATE TABLE artist_album (
+CREATE TABLE IF NOT EXISTS artist_album (
   artist_id INTEGER NOT NULL REFERENCES artist,
   album_id INTEGER NOT NULL REFERENCES album,
   PRIMARY KEY (artist_id, album_id)
@@ -89,9 +89,13 @@ CREATE OR REPLACE VIEW top_songs AS
    END;
    $set_0_value_as_null$ LANGUAGE plpgsql;
 
+DROP TRIGGER IF EXISTS set_year
+  ON public.album;
 
- CREATE TRIGGER set_year BEFORE INSERT OR UPDATE ON album
+CREATE TRIGGER set_year BEFORE INSERT OR UPDATE ON album
    FOR EACH ROW EXECUTE PROCEDURE set_year();
+
+CREATE INDEX IF NOT EXISTS song_name ON song USING spgist(name);
 
 
 
